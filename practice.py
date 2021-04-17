f423149bd8ad5ea1d6d3ca49c25e7945ab2c4d43
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-04-17 15:43:04 +0900

    practice if

---
+++ practice.py
@@ -0,0 +1,23 @@
+# if文
+x = 10
+if x < 0:
+    print('negative')
+elif x == 0:
+  print('zero')
+else:
+    print('positive')
+
+a = 5
+b = 10
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is positice')
+
+
+y = [1, 2, 3]
+x = 1
+if x in y:
+    print('in')
+if 100 not in y:
+    print('not in')
